193858b4db58dd4357e114de1b4e9e01e8839e46
Aaron Cepek <x@acepek.com>
2014-11-30 20:04:24 -0700

    Home ACF fields

--- wp-content/themes/dna-roots/templates/content-slider.php
+++ wp-content/themes/dna-roots/templates/content-slider.php
@@ -1,35 +1,73 @@
-<?php ?>
+<?php
+
+  if( have_rows( 'slides' ) ) {
+
+    $count = count ( get_field( 'slides' ) ) - 1;
+
+?>
 
 <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+
   <!-- Indicators -->
+
   <ol class="carousel-indicators">
-    <li data-target="#carousel" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel" data-slide-to="1"></li>
-    <li data-target="#carousel" data-slide-to="2"></li>
+
+<?php
+
+  while ( $count >= 0 ) {
+
+?>
+
+    <li data-target="#carousel" data-slide-to="<?php echo $count; ?>" class="<?php echo !$count ? 'active' : ''; ?>"></li>
+
+<?php
+
+  $count--;
+
+  }
+
+?>
+
   </ol>
+
+<?php
+
+    while ( have_rows( 'slides' ) ) {
+
+      the_row();
+
+      $title    = get_sub_field( 'title' );
+      $button   = get_sub_field( 'button' );
+      $href     = get_sub_field( 'href' );
+      $img      = get_sub_field( 'img' );
+
+?>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
+
     <div class="item active">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/banner/dna-slide_1.jpg">
+
+      <img src="<?php echo $img; ?>">
+
       <div class="carousel-caption">
-        <h3>Pain doesn’t have to be part of your daily routine.</h3>
-        <button type="button" class="btn btn-primary" href="#">Schedule an appointment today</button>
+
+        <h3><?php echo $title; ?></h3>
+
+        <button type="button" class="btn btn-primary" href="<?php echo $href; ?>"><?php echo $button; ?></button>
+
       </div>
+
     </div>
-    <div class="item">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/banner/dna-slide_2.jpg">
-      <div class="carousel-caption">
-        <h3>Six convenient locations. Peace of mind is close to home.</h3>
-        <button type="button" class="btn btn-primary" href="#">Our Locations</button>
-      </div>
-    </div>
-    <div class="item">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/banner/dna-slide_3.jpg">
-      <div class="carousel-caption">
-      	<h3>Focusing on the entire patient experience.</h3>
-        <button type="button" class="btn btn-primary" href="#">Learn about our physicians</button>
-      </div>
-    </div>
+
   </div>
+
 </div>
+
+<?php
+
+    }
+
+  }
+
+?>
--- wp-content/themes/dna-roots/templates/footer.php
+++ wp-content/themes/dna-roots/templates/footer.php
@@ -24,9 +24,9 @@
 
 			<div class="col-md-4 locations">
 
-				<h6>Our Locations</h6>
-
 				<ul class="row">
+
+					<h6>Our Locations</h6>
 
 					<li class="col-md-6">West Miffin, PA
 					<span>123-123-1234</span>
@@ -60,33 +60,33 @@
 
 			</div>
 
-			<div class="col-md-4 additional-info">
+			<div class="col-md-3 col-md-offset-1 additional-info">
 
 				<h6>Additional Information</h6>
 
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed lectus venenatis, suscipit urna a, consectetur lorem.</p>
 
-				<a class="btn btn-primary" href="#">Patient Portal</a>
+				<a class="btn btn-primary" href="#">Patient Portal <i class="fa fa-arrow-right"></i></a>
 
-				<a class="btn btn-primary" href="#">Employee Portal</a>
+				<a class="btn btn-primary" href="#">Employee Portal <i class="fa fa-arrow-right"></i></a>
 
-				<div>
+				<div class="">
 
-					<a href="#">
+					<a href="#" target="_blank">
 						<span class="fa-stack fa-lg">
 						  <i class="fa fa-circle fa-stack-2x"></i>
 						  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
 						</span>
 					</a>
 
-					<a href="#">
+					<a href="#" target="_blank">
 						<span class="fa-stack fa-lg">
 						  <i class="fa fa-circle fa-stack-2x"></i>
 						  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
 						</span>
 					</a>
 
-					<a href="#">
+					<a href="#" target="_blank">
 						<span class="fa-stack fa-lg">
 						  <i class="fa fa-circle fa-stack-2x"></i>
 						  <i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
