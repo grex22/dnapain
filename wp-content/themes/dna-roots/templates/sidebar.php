4014c261178a7a3ed68c767f02b9dc2e0e735102
Matt Keys <matt@mattkeys.me>
2014-12-03 17:31:49 -0500

    adding new blog sidebar, fixing home sidebar

--- wp-content/themes/dna-roots/templates/sidebar.php
+++ wp-content/themes/dna-roots/templates/sidebar.php
@@ -1,1 +1,8 @@
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<?php
+if ( is_front_page() ) {
+	get_template_part( 'templates/sidebar-front-page' );
+} elseif ( is_category() || is_archive() || is_singular('post') ) {
+	dynamic_sidebar( 'sidebar-blog' );
+} else {
+	dynamic_sidebar( 'sidebar-primary' );
+}
