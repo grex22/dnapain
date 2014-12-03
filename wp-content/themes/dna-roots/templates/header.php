694236a7ca2476355b2673503ba5900e735ca211
Greg Burkett <greg@ripfish.com>
2014-12-02 19:37:29 -0500

    garbage... delete me

--- wp-content/themes/dna-roots/templates/header.php
+++ wp-content/themes/dna-roots/templates/header.php
@@ -1,40 +1,30 @@
-<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+<header class="banner navbar navbar-fixed-top" id="top" role="banner">
   <div class="container">
-    <div class="col-md-12">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="navbar-header">
+    <div class="navbar-header">
+      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
 			<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-          </div>
-        </div>
-        <div class="col-lg-9 col-xs-6">
-            <div class="row">
-              <div class="col-lg-9">
-                <nav class="collapse navbar-collapse" role="navigation">
-                <?php
-                  if (has_nav_menu('primary_navigation')) :
-                    wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-                  endif;
-                ?>
-                </nav>
-              </div>
-              <div class="col-lg-3" id="cta">
-                <button type="button" class="btn btn-primary"  href="#">Schedule</button>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-                <a href="tel:1-866-580-7246" class="tel"><i class="fa fa-phone pull-left"></i> 1 866 580-PAIN</a>
-              </div>
-          </div>
-        </div>
-        
-      </div>
     </div>
+
+    <nav class="navbar-collapse bs-navbar-collapse collapse in" role="navigation" aria-expanded="true">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+    </nav>
+    <!--<div id="cta">
+      <button type="button" class="btn btn-primary"  href="#">Schedule</button>
+      <a href="tel:1-866-580-7246" class="tel"><i class="fa fa-phone pull-left"></i> 1 866 580-PAIN</a>
+    </div>
+    -->
   </div>
 </header>
+
 <?php
 
   if( is_front_page() ) {
