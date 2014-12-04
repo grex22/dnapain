a03aed606bb81b3a9ae8aee6d26e1ed7587ab785
Matt Keys <matt@mattkeys.me>
2014-12-04 00:34:43 -0500

    WP Nav Plus sidebar menu

--- wp-content/themes/dna-roots/templates/sidebar.php
+++ wp-content/themes/dna-roots/templates/sidebar.php
@@ -4,5 +4,25 @@ if ( is_front_page() ) {
 } elseif ( is_category() || is_archive() || is_singular('post') ) {
 	dynamic_sidebar( 'sidebar-blog' );
 } else {
+	global $post;
+	$parent = false;
+	$ancestors = get_ancestors( $post->ID, 'page' );
+	if ( ! empty( $ancestors ) ) :
+		$parent_id = $ancestors[0];
+		$parent = get_post( $parent_id );
+	endif;
+
+	if ( $parent && isset( $parent->post_title ) ) :
+		$menu_title = $parent->post_title;
+	else :
+		$menu_title = 'Menu';
+	endif;
+	?>
+	<section class="widget nav_plus_widget-2 widget_nav_plus_widget">
+		<h3><?php echo $menu_title; ?></h3>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'start_depth' => 1 ) ); ?>
+	</section>
+	<?php
+
 	dynamic_sidebar( 'sidebar-primary' );
 }
