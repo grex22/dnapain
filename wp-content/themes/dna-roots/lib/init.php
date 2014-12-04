f9c59bf50804fc057d9ac0c12d038d49df3a5372
Matt Keys <matt@mattkeys.me>
2014-12-04 01:41:24 -0500

    Locations sidebar

--- wp-content/themes/dna-roots/lib/init.php
+++ wp-content/themes/dna-roots/lib/init.php
@@ -81,6 +81,16 @@ function roots_widgets_init() {
     'before_title'  => '<h3>',
     'after_title'   => '</h3>',
   ));
+
+  
+  register_sidebar(array(
+    'name'          => __('Locations', 'roots'),
+    'id'            => 'sidebar-locations',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
 }
 add_action('widgets_init', 'roots_widgets_init');
 
--- wp-content/themes/dna-roots/templates/sidebar.php
+++ wp-content/themes/dna-roots/templates/sidebar.php
@@ -1,10 +1,38 @@
 <?php
 if ( is_front_page() ) {
+
 	get_template_part( 'templates/sidebar-front-page' );
+
 } elseif ( is_category() || is_archive() || is_singular('post') || is_home() ) {
+
 	dynamic_sidebar( 'sidebar-blog' );
-}elseif (is_page(array('contact','contact-us'))){
-  dynamic_sidebar( 'sidebar-contact' );
+
+} elseif ( is_page( array( 'contact', 'contact-us' ) ) ) {
+
+	dynamic_sidebar( 'sidebar-contact' );
+
+} elseif ( is_singular( 'locations' ) ) {
+	?>
+	<section class="widget">
+		<h3>Locations</h3>
+		<ul>
+		<?php
+		$args = array(
+			'post_type' => 'locations'
+		);
+		$locations = get_pages( $args );
+
+		foreach( $locations as $location ) {
+			?>
+			<li><a href="<?php echo get_permalink( $location->ID ); ?>"><?php echo $location->post_title; ?></a></li>
+			<?php
+		}
+		?>
+		</ul>
+	</section>
+	<?php
+	dynamic_sidebar( 'sidebar-locations' );
+
 } else {
 	global $post;
 	$parent = false;
