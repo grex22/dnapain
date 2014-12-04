f8aafbb78b67585f09368d0017d5dc7f2cb061c5
Greg Burkett <greg@ripfish.com>
2014-12-04 00:37:39 -0500

    new sidebar, header tweak for responsive

--- wp-content/themes/dna-roots/templates/sidebar.php
+++ wp-content/themes/dna-roots/templates/sidebar.php
@@ -3,6 +3,8 @@ if ( is_front_page() ) {
 	get_template_part( 'templates/sidebar-front-page' );
 } elseif ( is_category() || is_archive() || is_singular('post') ) {
 	dynamic_sidebar( 'sidebar-blog' );
+}elseif (is_page(array('contact','contact-us'))){
+  dynamic_sidebar( 'sidebar-contact' );
 } else {
 	dynamic_sidebar( 'sidebar-primary' );
 }
