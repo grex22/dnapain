92296ddde1afd77f1361daad3da0ae8266b6e9d1
Greg Burkett <greg@ripfish.com>
2014-12-04 11:50:46 -0500

    updating locations single display

--- wp-content/themes/dna-roots/templates/content-single-locations.php
+++ wp-content/themes/dna-roots/templates/content-single-locations.php
@@ -9,6 +9,48 @@
         the_post_thumbnail('large');
       }
       the_content();
+      
+      $clinic_address = get_field('clinic_address');
+      $phone = get_field('phone');
+      $fax = get_field('fax');
+      $directions = get_field('directions');
+      $google_map_link = get_field('google_map_link');
+      
+      if($clinic_address): ?>
+      
+      <h4>Address:</h4>
+      <?php echo wpautop($clinic_address); ?>
+      
+      <?php endif;
+      
+      if($phone): ?>
+      
+      <h4>Telephone:</h4>
+      <div><?php echo $phone; ?></div>
+      
+      <?php endif;
+      
+      if($fax): ?>
+      
+      <h4>Fax:</h4>
+      <div><?php echo $fax; ?></div>
+      
+      <?php endif;
+      
+      if($google_map_link): ?>
+      <hr>
+      <a href="<?php echo $google_map_link; ?>" class="btn btn-primary">View on Google Maps <i class="fa fa-globe"></i></a>
+      
+      <?php endif;
+      
+      if($directions): ?>
+      <hr>
+      <h4>Directions to this Clinic</h4>
+      
+      <?php echo $directions; ?>
+      
+      <?php endif;
+      
       ?>
     </div>
   </article>
