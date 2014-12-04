01ab93d23b024ba731116db85567242bfa32bcea
Matt Keys <matt@mattkeys.me>
2014-12-04 01:19:40 -0500

    removing social share and entry meta from single locations page

--- wp-content/themes/dna-roots/templates/content-single-locations.php
+++ wp-content/themes/dna-roots/templates/content-single-locations.php
@@ -2,7 +2,6 @@
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
       <?php 
@@ -11,22 +10,6 @@
       }
       the_content();
       ?>
-      <a class="btn btn-primary share-btn" href="">Share <i class="fa fa-share pull-right"></i></a>
-      <div class="social-shares">
-        <!-- AddToAny BEGIN -->
-        <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-          <a class="a2a_dd add2any_share" href="https://www.addtoany.com/share_save"></a>
-        </div>
-        <script type="text/javascript">
-        var a2a_config = a2a_config || {};
-        a2a_config.onclick = 1;
-        </script>
-        <script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>
-        <!-- AddToAny END -->
-      </div>
     </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-    </footer>
   </article>
 <?php endwhile; ?>
