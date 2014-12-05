7a62f71929eb0df43b886e7ae6c1d4ef4d41bce4
Greg Burkett <greg@ripfish.com>
2014-12-04 21:13:29 -0500

    3

--- wp-content/themes/dna-roots/assets/js/_main.js
+++ wp-content/themes/dna-roots/assets/js/_main.js
@@ -123,7 +123,19 @@ $(window).scroll(function(){
 });
 
 players = new Array();
+function onPlayerStateChange(event) {
 
+    if (event.data == YT.PlayerState.PLAYING) {
+        //alert(event.target.getVideoUrl());
+       // alert(players[0].getVideoUrl());
+        var temp = event.target.getVideoUrl();
+        var tempPlayers = $("iframe.yt_players");
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].getVideoUrl() != temp) players[i].stopVideo();
+
+        }
+    }
+}
 function onYouTubeIframeAPIReady() {
     var temp = $("iframe.yt_players");
     for (var i = 0; i < temp.length; i++) {
@@ -139,16 +151,3 @@ function onYouTubeIframeAPIReady() {
 onYouTubeIframeAPIReady();
 
 
-function onPlayerStateChange(event) {
-
-    if (event.data == YT.PlayerState.PLAYING) {
-        //alert(event.target.getVideoUrl());
-       // alert(players[0].getVideoUrl());
-        var temp = event.target.getVideoUrl();
-        var tempPlayers = $("iframe.yt_players");
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].getVideoUrl() != temp) players[i].stopVideo();
-
-        }
-    }
-}
