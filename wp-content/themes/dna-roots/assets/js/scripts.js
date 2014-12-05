f7ff79559e23476948dd367c19bd690345a58c40
Greg Burkett <greg@ripfish.com>
2014-12-04 21:31:01 -0500

    undoing video player stuff.. just not working

--- wp-content/themes/dna-roots/assets/js/scripts.js
+++ wp-content/themes/dna-roots/assets/js/scripts.js
@@ -2388,35 +2388,6 @@ var UTIL = {
 $(document).ready(UTIL.loadEvents);
 
 
-players = new Array();
-function onPlayerStateChange(event) {
-
-    if (event.data == YT.PlayerState.PLAYING) {
-        //alert(event.target.getVideoUrl());
-       // alert(players[0].getVideoUrl());
-        var temp = event.target.getVideoUrl();
-        var tempPlayers = $("iframe.yt_players");
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].getVideoUrl() != temp) players[i].stopVideo();
-
-        }
-    }
-}
-function onYouTubeIframeAPIReady() {
-    var temp = $("iframe.yt_players");
-    for (var i = 0; i < temp.length; i++) {
-        var t = new YT.Player($(temp[i]).attr('id'), {
-            events: {
-                'onStateChange': onPlayerStateChange
-            }
-        });
-        players.push(t);
-    }
-
-}
-onYouTubeIframeAPIReady();
-
-
 })(jQuery); // Fully reference jQuery after this point.
 
 $(function(){
--- wp-content/themes/dna-roots/templates/head.php
+++ wp-content/themes/dna-roots/templates/head.php
@@ -7,6 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
-  <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+  <!--<script type="text/javascript" src="http://www.youtube.com/player_api"></script>-->
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
