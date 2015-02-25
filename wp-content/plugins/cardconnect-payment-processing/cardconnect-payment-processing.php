d92cbee4be40182c34e990ea68a6f17d6a30aa8b
Matt Keys <matt@mattkeys.me>
2015-02-24 23:45:38 -0500

    removing unneeded constant

--- wp-content/plugins/cardconnect-payment-processing/cardconnect-payment-processing.php
+++ wp-content/plugins/cardconnect-payment-processing/cardconnect-payment-processing.php
@@ -18,9 +18,5 @@ if ( ! defined( 'CCPP_PLUGIN_DIR' ) ) {
 	define( 'CCPP_PLUGIN_DIR', dirname( __FILE__ ) );
 }
 
-//Publicly Accessible path
-if ( ! defined( 'CCPP_PUBLIC_PATH' ) ) {
-	define( 'CCPP_PUBLIC_PATH', plugin_dir_url( __FILE__ ) );
-}
 
 require 'core/class-CCPP-Core.php';
